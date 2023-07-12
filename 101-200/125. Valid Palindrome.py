e7e0860178830911f37719b4eb23942897bca689
Denys Kalinovskyi <ipzk211_kdyu@student.ztu.edu.ua>
2023-07-13 00:09:11 +0300

    125

---
+++ 101-200/125. Valid Palindrome.py
@@ -0,0 +1,20 @@
+import re
+from string import ascii_lowercase
+
+
+def solve(s: str):
+    pattern = r'[^a-z0-9]'
+    s = re.sub(pattern, '', s.lower())
+
+    i, j = 0, len(s) - 1
+    while i < len(s) and j >= 0:
+        if s[i] != s[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+
+
+assert solve('A man, a plan, a canal: Panama')
+assert solve('race a car') is False
+assert solve('0F') is False
